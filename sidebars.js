9efb4bf7c98588aadeb6d605de450f389ad7c266
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-03-19 14:57:53 +0000

    feat(): add readme

--- sidebars.js
+++ sidebars.js
@@ -1,15 +1,16 @@
 module.exports = {
   docs: [
     {
-      type: 'category',
-      label: 'Docusaurus Tutorial',
+      type: "category",
+      label: "Docusaurus Tutorial",
       items: [
-        'getting-started',
-        'create-a-page',
-        'create-a-document',
-        'create-a-blog-post',
-        'markdown-features',
-        'thank-you',
+        "README",
+        "getting-started",
+        "create-a-page",
+        "create-a-document",
+        "create-a-blog-post",
+        "markdown-features",
+        "thank-you",
       ],
     },
   ],
