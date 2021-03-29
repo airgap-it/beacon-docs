52048d6129ceb7c4f97e743e173e74ef2de63921
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-03-29 18:33:31 +0000

    feat(docs): extract run code

--- src/components/RunnableCode.tsx
+++ src/components/RunnableCode.tsx
@@ -1,8 +1,4 @@
-import * as beacon from "@airgap/beacon-sdk";
-import * as ts from "typescript";
-
-import * as taquito from "@taquito/taquito";
-import * as taquitoWallet from "@taquito/beacon-wallet";
+import { DAppClient } from "@airgap/beacon-sdk";
 
 import React, { useState } from "react";
 import Tabs from "@theme/Tabs";
@@ -10,92 +6,7 @@ import TabItem from "@theme/TabItem";
 import BrowserWindow from "./BrowserWindow/BrowserWindow";
 import Monaco from "./Monaco";
 import LoadingAnimation from "./LoadingAnimation";
-
-function replaceAll(string: string, search: string, replace: string) {
-  return string.split(search).join(replace);
-}
-
-const removeImports = (code: string) => {
-  const lines = code.split("\n");
-  let include = true;
-
-  return lines
-    .map((l) => {
-      if (l.trim().startsWith("import")) {
-        include = false;
-      }
-
-      const out = include ? l : undefined;
-
-      if (l.indexOf("@airgap/beacon-sdk") >= 0 || l.indexOf("@taquito") >= 0) {
-        include = true;
-      }
-
-      return out;
-    })
-    .filter((l) => !!l)
-    .join("\n");
-};
-
-const run = (rawCode: string, setOutput: (str: string) => void) => {
-  let code = rawCode;
-
-  let output = "";
-  const appendOutput = (str: string) => {
-    output += "\n" + str;
-    setOutput(output.trim());
-  };
-
-  const myLog = (...args: any[]) => {
-    appendOutput(args.join(" "));
-    console.log(...args);
-  };
-
-  code = replaceAll(code, "console.log(", "progress(");
-  code = removeImports(code);
-  code = ts.transpile(`({
-    run: async (beacon: any, taquito: any, taquitoWallet: any, progress: any): string => {
-      Object.keys(beacon).forEach(key => {
-        window[key] = beacon[key]
-      })
-      Object.keys(taquito).forEach(key => {
-        window[key] = taquito[key]
-      })
-      Object.keys(taquitoWallet).forEach(key => {
-        window[key] = taquitoWallet[key]
-      })
-      return (async () => {
-        ${code};
-        if (typeof result !== 'undefined') {
-          return result
-        }
-      })()
-    })`);
-  let runnable: any;
-  // console.log("TRANSPILED code", code);
-  return new Promise((resolve) => {
-    try {
-      runnable = eval(code);
-      runnable
-        .run(beacon, taquito, taquitoWallet, myLog)
-        .then((result: string) => {
-          if (result) {
-            appendOutput("Returned:\n" + JSON.stringify(result, null, 2));
-          }
-          resolve(result);
-        })
-        .catch((err: any) => {
-          console.warn(err);
-          appendOutput(JSON.stringify(err, null, 2));
-          resolve(err);
-        });
-    } catch (e) {
-      appendOutput(e);
-      console.error(e);
-      resolve(e);
-    }
-  });
-};
+import { runBeaconCode } from "../utils";
 
 enum ExecutionState {
   INIT,
@@ -114,12 +25,12 @@ const Child = ({ code }) => {
     }
     await clear();
     setExecutionState(ExecutionState.STARTED);
-    await run(code.props.children.props.children, setOutput);
+    await runBeaconCode(code.props.children.props.children, setOutput);
     setExecutionState(ExecutionState.ENDED);
   };
   const reset = async () => {
     clear();
-    const dAppClient = new beacon.DAppClient({ name: "Cleanup" });
+    const dAppClient = new DAppClient({ name: "Cleanup" });
     await dAppClient.destroy();
   };
   const clear = async () => {
---
+++ src/utils.ts
@@ -0,0 +1,91 @@
+import * as beacon from "@airgap/beacon-sdk";
+import * as ts from "typescript";
+
+import * as taquito from "@taquito/taquito";
+import * as taquitoWallet from "@taquito/beacon-wallet";
+
+function replaceAll(string: string, search: string, replace: string) {
+    return string.split(search).join(replace);
+}
+
+const removeImports = (code: string) => {
+    const lines = code.split("\n");
+    let include = true;
+
+    return lines
+        .map((l) => {
+            if (l.trim().startsWith("import")) {
+                include = false;
+            }
+
+            const out = include ? l : undefined;
+
+            if (l.indexOf("@airgap/beacon-sdk") >= 0 || l.indexOf("@taquito") >= 0) {
+                include = true;
+            }
+
+            return out;
+        })
+        .filter((l) => !!l)
+        .join("\n");
+};
+
+export const runBeaconCode = (rawCode: string, setOutput: (str: string) => void) => {
+    let code = rawCode;
+
+    let output = "";
+    const appendOutput = (str: string) => {
+        output += "\n" + str;
+        setOutput(output.trim());
+    };
+
+    const myLog = (...args: any[]) => {
+        appendOutput(args.join(" "));
+        console.log(...args);
+    };
+
+    code = replaceAll(code, "console.log(", "progress(");
+    code = removeImports(code);
+    code = ts.transpile(`({
+      run: async (beacon: any, taquito: any, taquitoWallet: any, progress: any): string => {
+        Object.keys(beacon).forEach(key => {
+          window[key] = beacon[key]
+        })
+        Object.keys(taquito).forEach(key => {
+          window[key] = taquito[key]
+        })
+        Object.keys(taquitoWallet).forEach(key => {
+          window[key] = taquitoWallet[key]
+        })
+        return (async () => {
+          ${code};
+          if (typeof result !== 'undefined') {
+            return result
+          }
+        })()
+      })`);
+    let runnable: any;
+    // console.log("TRANSPILED code", code);
+    return new Promise((resolve) => {
+        try {
+            runnable = eval(code);
+            runnable
+                .run(beacon, taquito, taquitoWallet, myLog)
+                .then((result: string) => {
+                    if (result) {
+                        appendOutput("Returned:\n" + JSON.stringify(result, null, 2));
+                    }
+                    resolve(result);
+                })
+                .catch((err: any) => {
+                    console.warn(err);
+                    appendOutput(JSON.stringify(err, null, 2));
+                    resolve(err);
+                });
+        } catch (e) {
+            appendOutput(e);
+            console.error(e);
+            resolve(e);
+        }
+    });
+};
