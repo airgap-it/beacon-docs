6515d9723790904f9f6b019a2be926f60972a49a
Isacco <i.sordo@papers.ch>
2023-09-14 17:16:58 +0200

    feat: updated docs

---
+++ src/examples/getting-started-active-account-events.beacon.ts
@@ -0,0 +1,25 @@
+/// START
+import { BeaconEvent, DAppClient } from "@airgap/beacon-sdk";
+/// END
+
+async () => {
+  /// START
+  const dAppClient = new DAppClient({ name: "Beacon Docs" });
+
+  // Listen for all the active account changes
+  dAppClient.subscribeToEvent(
+    BeaconEvent.ACTIVE_ACCOUNT_SET,
+    async (account) => {
+      // An active account has been set, update the dApp UI
+      console.log(`${BeaconEvent.ACTIVE_ACCOUNT_SET} triggered: `, account);
+    }
+  );
+
+  // Check if we are connected. If not, do a permission request first.
+  const activeAccount = await dAppClient.getActiveAccount();
+  if (!activeAccount) {
+    await dAppClient.requestPermissions();
+  }
+
+  /// END
+};
---
+++ src/examples/getting-started-active-account-events.taquito.ts
@@ -0,0 +1,20 @@
+/// START
+import { TezosToolkit } from "@taquito/taquito";
+import { BeaconWallet } from "@taquito/beacon-wallet";
+import { BeaconEvent } from "@airgap/beacon-sdk";
+/// END
+
+async () => {
+  /// START
+  const Tezos = new TezosToolkit("https://mainnet-tezos.giganode.io");
+  const wallet = new BeaconWallet({ name: "Beacon Docs Taquito" });
+
+  Tezos.setWalletProvider(wallet);
+
+  wallet.client.subscribeToEvent(BeaconEvent.ACTIVE_ACCOUNT_SET, (account) => {
+    // An active account has been set
+    console.log(`${BeaconEvent.ACTIVE_ACCOUNT_SET} triggered: `, account);
+  });
+
+  /// END
+};
